f8c4ca7d9385d3e53ebb2be45b7302b0308135fa
Matias Novillo <novillo.matias1@gmail.com>
2023-02-14 12:31:57 -0300

    Se hicieron correcciones y mejoras en Planet

--- FiyiRequirements/Areas/BasicCulture/Pages/PlanetNonQueryPage.cshtml.cs
+++ FiyiRequirements/Areas/BasicCulture/Pages/PlanetNonQueryPage.cshtml.cs
@@ -9,14 +9,14 @@ using System.Collections.Generic;
  * GUID:e6c09dfe-3a3e-461b-b3f9-734aee05fc7b
  * 
  * Coded by fiyistack.com
- * Copyright © 2022
+ * Copyright © 2023
  * 
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  * 
  */
 
-//Last modification on: 21/12/2022 10:33:36
+//Last modification on: 14/02/2023 12:24:52
 
 namespace FiyiRequirements.Areas.BasicCulture.Pages
 {
@@ -24,10 +24,10 @@ namespace FiyiRequirements.Areas.BasicCulture.Pages
     /// Stack:             9 <br/>
     /// Name:              C# Razor Page. <br/>
     /// Function:          Allow you to show HTML files using Razor Page technology. <br/>
-    /// Last modification: 21/12/2022 10:33:36
+    /// Last modification: 14/02/2023 12:24:52
     /// </summary>
     [PlanetFilter]
-    public partial class PagePlanetNonQueryModel : PageModel
+    public partial class PlanetNonQueryPageModel : PageModel
     {
         public void OnGet()
         {
