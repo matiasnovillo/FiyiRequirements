3c686903fe7267014ccdd03e05dc944cd47cb298
Matias Novillo <novillo.matias1@gmail.com>
2023-02-14 14:56:01 -0300

    Se hicieron mejoras y correcciones en User

--- FiyiRequirements/wwwroot/js/CMSCore/User/jQuery/UserNonQuery_jQuery.js
+++ FiyiRequirements/wwwroot/js/CMSCore/User/jQuery/UserNonQuery_jQuery.js
@@ -4,7 +4,7 @@
  * GUID:e6c09dfe-3a3e-461b-b3f9-734aee05fc7b
  * 
  * Coded by fiyistack.com
- * Copyright © 2022
+ * Copyright © 2023
  * 
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
@@ -13,10 +13,10 @@
 
 //Stack: 10
 
-//Last modification on: 21/12/2022 11:12:12
+//Last modification on: 14/02/2023 14:44:39
 
 $(document).ready(function () {
-    //Employee select tag
+    //User select tag
     $("#cmscore-user-roleid-select").on("change", function (e) {
         $("#cmscore-user-roleid-list").html(`<li class="nav-item">
             <a class="nav-link mb-sm-3 mb-md-0 active" data-toggle="tab" href="javascript:void(0)" role="tab" aria-controls="" aria-selected="true">
@@ -55,56 +55,18 @@ $("#cmscore-user-insert-or-update-button").on("click", function (e) {
     var xmlHttpRequest = new XMLHttpRequest();
     //Set event listeners
     xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
-        //Show success button and success message modal
-        $("#cmscore-user-insert-or-update-message").addClass("btn-secondary");
-        $("#cmscore-user-insert-or-update-message").removeClass("btn-success");
-        $("#cmscore-user-insert-or-update-message").removeClass("btn-error");
-        $("#cmscore-user-insert-or-update-message").removeAttr("data-toggle");
-        $("#cmscore-user-insert-or-update-message").removeAttr("data-target");
-        $("#cmscore-user-insert-or-update-message").html(`Sending data. Please, wait`);
-    });
-    xmlHttpRequest.upload.addEventListener("progress", function (e) {
-        // While sending and loading data.
-    });
-    xmlHttpRequest.upload.addEventListener("load", function (e) {
-        // When the request has successfully completed.
-    });
-    xmlHttpRequest.upload.addEventListener("loadend", function (e) {
-        // When the request has completed (either in success or failure).
-    });
-    xmlHttpRequest.upload.addEventListener("error", function (e) {
-        // When the request has failed.
-    });
-    xmlHttpRequest.upload.addEventListener("abort", function (e) {
-        // When the request has been aborted. 
-    });
-    xmlHttpRequest.upload.addEventListener("timeout", function (e) {
-        // When the author specified timeout has passed before the request could complete
+        //SAVING
+        $.notify({message: "Saving data. Please, wait"}, {type: "info", placement: { from: "bottom", align: "center" }});
     });
     xmlHttpRequest.onload = function () {
-        console.log(xmlHttpRequest);
         if (xmlHttpRequest.status >= 400) {
-            //Show error button and error message modal
-            $("#cmscore-user-insert-or-update-message").addClass("btn-danger");
-            $("#cmscore-user-insert-or-update-message").removeClass("btn-success");
-            $("#cmscore-user-insert-or-update-message").removeClass("btn-secondary");
-            $("#cmscore-user-insert-or-update-message").attr("data-toggle", "modal");
-            $("#cmscore-user-insert-or-update-message").attr("data-target", "#cmscore-user-error-message-modal");
-            $("#cmscore-user-insert-or-update-message").html(`<i class="fas fa-exclamation-triangle"></i> 
-                                                                There was an error while sending the data`);
-            $("#cmscore-user-error-message-title").html("There was an error while sending the data");
-            $("#cmscore-user-error-message-text").html(xmlHttpRequest.response);
-            console.log("Error:" + xmlHttpRequest.response);
+            //ERROR
+            console.log(xmlHttpRequest);
+            $.notify({ icon: "fas fa-exclamation-triangle", message: "There was an error while saving the data" }, { type: "danger", placement: { from: "bottom", align: "center"}});
         }
         else {
-            //Show success button
-            $("#cmscore-user-insert-or-update-message").addClass("btn-success");
-            $("#cmscore-user-insert-or-update-message").removeClass("btn-error");
-            $("#cmscore-user-insert-or-update-message").removeClass("btn-secondary");
-            $("#cmscore-user-insert-or-update-message").removeAttr("data-toggle");
-            $("#cmscore-user-insert-or-update-message").removeAttr("data-target");
-            $("#cmscore-user-insert-or-update-message").html(`<i class="fas fa-check"></i>
-                                                                Data sent successfully`);
+            //SUCCESS
+            $.notify({ icon: "fas fa-check", message: "Data sent successfully"}, { type: "success", placement: { from: "bottom", align: "center"}});
         }
     };
     //Open connection
