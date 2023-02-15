34832f4cd1f0a1abeef53fa35500706eff233500
Matias Novillo <novillo.matias1@gmail.com>
2023-02-15 17:47:09 -0300

    Se mejoro Country

--- FiyiRequirements/wwwroot/js/BasicCulture/Country/jQuery/CountryNonQuery_jQuery.js
+++ FiyiRequirements/wwwroot/js/BasicCulture/Country/jQuery/CountryNonQuery_jQuery.js
@@ -13,9 +13,20 @@
 
 //Stack: 10
 
-//Last modification on: 14/02/2023 11:58:40
+//Last modification on: 15/02/2023 17:41:07
 
+//Create a formdata object
+var formData = new FormData();
+
+//Used for Quill Editor
+
+
+//Used for file input
+
+
+//LOAD EVENT
 $(document).ready(function () {
+
     //PlanetId select tag
     $("#basicculture-country-planetid-select").on("change", function (e) {
         $("#basicculture-country-planetid-list").html(`<li class="nav-item">
@@ -25,51 +36,56 @@ $(document).ready(function () {
             <input type="hidden" id="basicculture-country-planetid-input" value="${$("#basicculture-country-planetid-select option:selected").val()}"/>
         </li>`);
     });
-});
-
-//Used for Quill Editor
-
-
-//Used for file input
-
-
-//Create a formdata object
-var formData = new FormData();
-$("#basicculture-country-insert-or-update-button").on("click", function (e) {
-    //Stop stuff happening
-    e.stopPropagation();
-    e.preventDefault();
-
-    //Add or edit value
-    formData.append("basicculture-country-title-page", $("#basicculture-country-title-page").html());
-    formData.append("basicculture-country-countryid-input", $("#basicculture-country-countryid-input").val());
-
-    formData.append("basicculture-country-name-input", $("#basicculture-country-name-input").val());
-    formData.append("basicculture-country-geographicalcoordinates-input", $("#basicculture-country-geographicalcoordinates-input").val());
-    formData.append("basicculture-country-code-input", $("#basicculture-country-code-input").val());
-    formData.append("basicculture-country-planetid-input", $("#basicculture-country-planetid-input").val());
     
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName("needs-validation");
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener("submit", function (event) {
 
-    //Setup request
-    var xmlHttpRequest = new XMLHttpRequest();
-    //Set event listeners
-    xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
-        //SAVING
-        $.notify({message: "Saving data. Please, wait"}, {type: "info", placement: { from: "bottom", align: "center" }});
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (form.checkValidity() === true) {
+                
+                //CountryId
+                formData.append("basicculture-country-countryid-input", $("#basicculture-country-countryid-input").val());
+
+                formData.append("basicculture-country-name-input", $("#basicculture-country-name-input").val());
+                formData.append("basicculture-country-geographicalcoordinates-input", $("#basicculture-country-geographicalcoordinates-input").val());
+                formData.append("basicculture-country-code-input", $("#basicculture-country-code-input").val());
+                formData.append("basicculture-country-planetid-input", $("#basicculture-country-planetid-input").val());
+                
+
+                //Setup request
+                var xmlHttpRequest = new XMLHttpRequest();
+                //Set event listeners
+                xmlHttpRequest.upload.addEventListener("loadstart", function (e) {
+                    //SAVING
+                    $.notify({ message: "Saving data. Please, wait" }, { type: "info", placement: { from: "bottom", align: "center" } });
+                });
+                xmlHttpRequest.onload = function () {
+                    if (xmlHttpRequest.status >= 400) {
+                        //ERROR
+                        console.log(xmlHttpRequest);
+                        $.notify({ icon: "fas fa-exclamation-triangle", message: "There was an error while saving the data" }, { type: "danger", placement: { from: "bottom", align: "center" } });
+                    }
+                    else {
+                        //SUCCESS
+                        $.notify({ icon: "fas fa-check", message: "Data sent successfully" }, { type: "success", placement: { from: "bottom", align: "center" } });
+                    }
+                };
+                //Open connection
+                xmlHttpRequest.open("POST", "/api/BasicCulture/Country/1/InsertOrUpdateAsync", true);
+                //Send request
+                xmlHttpRequest.send(formData);
+            }
+            else {
+                $.notify({ message: "Please, complete all fields." }, { type: "warning", placement: { from: "bottom", align: "center" } });
+            }
+
+
+            form.classList.add("was-validated");
+        }, false);
     });
-    xmlHttpRequest.onload = function () {
-        if (xmlHttpRequest.status >= 400) {
-            //ERROR
-            console.log(xmlHttpRequest);
-            $.notify({ icon: "fas fa-exclamation-triangle", message: "There was an error while saving the data" }, { type: "danger", placement: { from: "bottom", align: "center"}});
-        }
-        else {
-            //SUCCESS
-            $.notify({ icon: "fas fa-check", message: "Data sent successfully"}, { type: "success", placement: { from: "bottom", align: "center"}});
-        }
-    };
-    //Open connection
-    xmlHttpRequest.open("POST", "/api/BasicCulture/Country/1/InsertOrUpdateAsync", true);
-    //Send request
-    xmlHttpRequest.send(formData);
 });
