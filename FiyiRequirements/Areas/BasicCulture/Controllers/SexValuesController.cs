99d2915d453880031f6007ed1e2cb0845a7385af
Matias Novillo <novillo.matias1@gmail.com>
2023-02-21 17:55:53 -0300

    Se hicieron correcciones/mejoras en algunas clases de Sex

--- FiyiRequirements/Areas/BasicCulture/Controllers/SexValuesController.cs
+++ FiyiRequirements/Areas/BasicCulture/Controllers/SexValuesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using FiyiRequirements.Areas.BasicCore.Models;
+using FiyiRequirements.Areas.BasicCulture.DTOs;
 using FiyiRequirements.Areas.BasicCulture.Filters;
 using FiyiRequirements.Areas.BasicCulture.Protocols;
 using FiyiRequirements.Areas.BasicCulture.Models;
@@ -24,7 +25,7 @@ using System.IO;
  * 
  */
 
-//Last modification on: 15/02/2023 18:02:20
+//Last modification on: 21/02/2023 17:54:25
 
 namespace FiyiRequirements.Areas.BasicCulture.Controllers
 {
@@ -32,7 +33,7 @@ namespace FiyiRequirements.Areas.BasicCulture.Controllers
     /// Stack:             6<br/>
     /// Name:              C# Web API Controller. <br/>
     /// Function:          Allow you to intercept HTPP calls and comunicate with his C# Service using dependency injection.<br/>
-    /// Last modification: 15/02/2023 18:02:20
+    /// Last modification: 21/02/2023 17:54:25
     /// </summary>
     [ApiController]
     [SexFilter]
@@ -113,14 +114,14 @@ namespace FiyiRequirements.Areas.BasicCulture.Controllers
         }
 
         [HttpPost("~/api/BasicCulture/Sex/1/SelectAllPagedToJSON")]
-        public sexModelQuery SelectAllPagedToJSON([FromBody] sexModelQuery sexModelQuery)
+        public sexSelectAllPaged SelectAllPagedToJSON([FromBody] sexSelectAllPaged sexSelectAllPaged)
         {
             try
             {
                 var SyncIO = HttpContext.Features.Get<IHttpBodyControlFeature>();
                 if (SyncIO != null) { SyncIO.AllowSynchronousIO = true; }
 
-                 return _SexProtocol.SelectAllPagedToModel(sexModelQuery);
+                 return _SexProtocol.SelectAllPagedToModel(sexSelectAllPaged);
             }
             catch (Exception ex)
             {
