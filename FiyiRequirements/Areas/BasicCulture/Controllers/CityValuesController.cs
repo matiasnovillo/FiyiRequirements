dd849c4f44b8dd14f7eff421b80da070ee072158
Matias Novillo <novillo.matias1@gmail.com>
2023-02-21 17:44:50 -0300

    Se hicieron mejoras/correcciones en algunas clases de City

--- FiyiRequirements/Areas/BasicCulture/Controllers/CityValuesController.cs
+++ FiyiRequirements/Areas/BasicCulture/Controllers/CityValuesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using FiyiRequirements.Areas.BasicCore.Models;
+using FiyiRequirements.Areas.BasicCulture.DTOs;
 using FiyiRequirements.Areas.BasicCulture.Filters;
 using FiyiRequirements.Areas.BasicCulture.Protocols;
 using FiyiRequirements.Areas.BasicCulture.Models;
@@ -24,7 +25,7 @@ using System.IO;
  * 
  */
 
-//Last modification on: 15/02/2023 17:55:57
+//Last modification on: 21/02/2023 17:42:15
 
 namespace FiyiRequirements.Areas.BasicCulture.Controllers
 {
@@ -32,7 +33,7 @@ namespace FiyiRequirements.Areas.BasicCulture.Controllers
     /// Stack:             6<br/>
     /// Name:              C# Web API Controller. <br/>
     /// Function:          Allow you to intercept HTPP calls and comunicate with his C# Service using dependency injection.<br/>
-    /// Last modification: 15/02/2023 17:55:57
+    /// Last modification: 21/02/2023 17:42:15
     /// </summary>
     [ApiController]
     [CityFilter]
@@ -113,14 +114,14 @@ namespace FiyiRequirements.Areas.BasicCulture.Controllers
         }
 
         [HttpPost("~/api/BasicCulture/City/1/SelectAllPagedToJSON")]
-        public cityModelQuery SelectAllPagedToJSON([FromBody] cityModelQuery cityModelQuery)
+        public citySelectAllPaged SelectAllPagedToJSON([FromBody] citySelectAllPaged citySelectAllPaged)
         {
             try
             {
                 var SyncIO = HttpContext.Features.Get<IHttpBodyControlFeature>();
                 if (SyncIO != null) { SyncIO.AllowSynchronousIO = true; }
 
-                 return _CityProtocol.SelectAllPagedToModel(cityModelQuery);
+                 return _CityProtocol.SelectAllPagedToModel(citySelectAllPaged);
             }
             catch (Exception ex)
             {
